d3ee64f6b616a444c7333615b44fea9ec093a40d
Sone <sthaya@gmail.com>
2021-01-31 17:31:43 -0800

    added svg background and able to update post

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Grid, CircularProgress } from "@material-ui/core";
 import { useSelector } from "react-redux";
+
 import Post from "./Post/Post";
 import useStyles from "./styles";
 
-const Posts = () => {
+const Posts = ({ setCurrentId }) => {
   const posts = useSelector((state) => state.posts);
   const classes = useStyles();
 
@@ -18,8 +19,8 @@ const Posts = () => {
       spacing={3}
     >
       {posts.map((post) => (
-        <Grid item key={post._id} xs={12} sm={6}>
-          <Post post={post} />
+        <Grid key={post._id} item xs={12} sm={6} md={6}>
+          <Post post={post} setCurrentId={setCurrentId} />
         </Grid>
       ))}
     </Grid>
