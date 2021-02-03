52c8807427c59979a02dcb1ec58cf2eaae3ee765
Sone <sthaya@gmail.com>
2021-02-02 18:36:28 -0800

    created styles file for auth component

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,14 +1,14 @@
-import React, { useState, useEffect } from "react";
-import { Container, Grow, Grid } from "@material-ui/core";
+import React, { useState, useEffect } from 'react';
+import { Container, Grow, Grid } from '@material-ui/core';
+import { useDispatch } from 'react-redux';
 
-import { getPosts } from "../../actions/posts";
-import Posts from "../Posts/Posts";
-import Form from "../Form/Form";
+import { getPosts } from '../../actions/posts';
+import Posts from '../Posts/Posts';
+import Form from '../Form/Form';
 
 const Home = () => {
   const [currentId, setCurrentId] = useState(0);
   const dispatch = useDispatch();
-  const classes = useStyles();
 
   useEffect(() => {
     dispatch(getPosts());
@@ -17,13 +17,7 @@ const Home = () => {
   return (
     <Grow in>
       <Container>
-        <Grid
-          container
-          className={classes.mainContainer}
-          justify="space-between"
-          alignItems="stretch"
-          spacing={3}
-        >
+        <Grid container justify="space-between" alignItems="stretch" spacing={3}>
           <Grid item xs={12} sm={7}>
             <Posts setCurrentId={setCurrentId} />
           </Grid>
@@ -35,5 +29,4 @@ const Home = () => {
     </Grow>
   );
 };
-
 export default Home;
