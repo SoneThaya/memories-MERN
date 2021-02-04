0f8338bfc84f6957e25f9587d2ee00ec0936c6ed
Sone <sthaya@gmail.com>
2021-02-03 20:35:45 -0800

    fixed typo in api index file, user now able to post

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,4 +1,5 @@
 import express from "express";
+
 import {
   getPosts,
   createPost,
@@ -6,9 +7,9 @@ import {
   likePost,
   deletePost,
 } from "../controllers/posts.js";
-import auth from "../middleware/auth.js";
 
 const router = express.Router();
+import auth from "../middleware/auth.js";
 
 router.get("/", getPosts);
 router.post("/", auth, createPost);
